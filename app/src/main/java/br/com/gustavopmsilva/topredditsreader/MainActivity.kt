741c02c637bb92ce669c83cad2e17811b1aa2e8e
gustavopmsilva <gustavopmirandasilva@gmail.com>
2022-01-10 10:56:49 -0300

    Changed app to use navigation

--- app/src/main/java/br/com/gustavopmsilva/topredditsreader/MainActivity.kt
+++ app/src/main/java/br/com/gustavopmsilva/topredditsreader/MainActivity.kt
@@ -8,13 +8,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.main_activity)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, ListFragment.newInstance())
-                .commitNow()
-        }
     }
 }
